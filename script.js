bd704de3a54b3bf982f256a26f2663073c346ef5
Christian <christiantisby1@gmail.com>
2018-12-20 23:38:05 -0500

    final commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// var answer = prompt("are we there yet?")
+
+// while(answer !== "yes" && answer !== "yeah") {
+//   var answer = prompt("are we there yet")
+// }
+
+// alert("YAY, WE MADE IT!!!")
+
+// if(answer === "yes") {
+//   alert("YAY, We made it!")
+// } else {
+//   var answer = prompt("are we there yet?")
+// }
+
+var answer = prompt("are we there yet?")
+
+while(answer.indexOf("yes") === -1) {
+  var answer = prompt("are we there yet")
+}
+
+alert("YAY, WE MADE IT!!!")
